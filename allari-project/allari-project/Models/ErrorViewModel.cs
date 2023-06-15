61416675dc1da52a29a3663ed59dfe8da1946a30
Antonio Correia <antonio.alves.correia@gmail.com>
2023-06-15 20:51:56 -0300

    adding first version
    - creating .net core mvc project
    - adding header and navigation menu as asked
    - tracking mouse position function
    - footer
    - digital clock

---
+++ allari-project/allari-project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace allari_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ allari-project/allari-project/wwwroot/js/site.js
@@ -0,0 +1,36 @@
+﻿
+function showClock() {
+    var date = new Date();
+    var h = date.getHours(); 
+    var m = date.getMinutes(); 
+    var s = date.getSeconds(); 
+    
+    if (h == 0) {
+        h = 12;
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s;
+    document.getElementById("digital-clock").innerText = time;
+    document.getElementById("digital-clock").textContent = time;
+
+    setTimeout(showClock, 1000);
+}
+
+showClock();
+
+
+onmousemove = function(e) {
+    var posX = e.clientX;
+    var posY = e.clientY;
+
+    document.getElementById("mouse-x").innerText = posX;
+    document.getElementById("mouse-x").textContent = posX;
+
+    document.getElementById("mouse-y").innerText = posY;
+    document.getElementById("mouse-y").textContent = posY;
+
+}
