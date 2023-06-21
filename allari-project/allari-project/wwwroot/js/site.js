9c431568ea6b20a181fafa3bd4790e2ff55a55a4
Antonio Correia <antonio.alves.correia@gmail.com>
2023-06-20 21:01:27 -0300

    adding style to plant table
    - layout tweaks
    - adding ripple effect to button
    - more json entries

--- allari-project/allari-project/wwwroot/js/site.js
+++ allari-project/allari-project/wwwroot/js/site.js
@@ -63,10 +63,30 @@ function loadPlantsList() {
         }
     });
 }
-function loadPlantsListFetch() {
-    fetch('http://yourapi.com/data')
-        .then(response => {
-            console.log(response)
-        });
+
+
+function createRipple(event) {
+    const button = event.currentTarget;
+
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.clientX - (button.offsetLeft + radius)}px`;
+    circle.style.top = `${event.clientY - (button.offsetTop + radius)}px`;
+    circle.classList.add("ripple");
+
+    const ripple = button.getElementsByClassName("ripple")[0];
+
+    if (ripple) {
+        ripple.remove();
+    }
+
+    button.appendChild(circle);
 }
-    
+
+const rippleButtons = document.getElementsByClassName("btn-ripple");
+for (const button of rippleButtons) {
+    button.addEventListener("click", createRipple);
+}
